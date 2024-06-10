33f59f1213393e68d55e464e0438aac401bf8986
alisbah6 <alisbahhina@gmail.com>
2024-06-10 23:51:02 +0530

    style_updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './user/Login.jsx';
+import Register from './user/Register.jsx';
+
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Routes>
+      <Route path='/Login' element={<Login/>}/>
+      <Route path='/Register' element={<Register/>}/>
+      </Routes>
     </div>
   );
 }
