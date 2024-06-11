70e565074a170522f613cf737fca341459c2dd40
Aman Bhatt <22680764+notfound07@users.noreply.github.com>
2024-06-11 16:07:14 +0530

    Responsive nav Done

--- client/src/Nav-Foot/Navbar.jsx
+++ client/src/Nav-Foot/Navbar.jsx
@@ -1,24 +1,49 @@
 import './Nav-Foot.css';
-import React from 'react';
+import logo from '../Assets/logo.png'; // Import the image
+import { useState, useEffect } from 'react';
 
 function Navbar() {
+    
+    const [dropdownVisible, setDropdownVisible] = useState(false);
+    const toggleDropdown = () => {
+        setDropdownVisible(!dropdownVisible);
+    };
+
+    useEffect(() => {
+        const handleResize = () => {
+            // Hide dropdown when screen size increases beyond 799px
+            if (window.innerWidth > 799) {
+                setDropdownVisible(false);
+            }
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
-        <div class="frame">
+        <div className="frame">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"></link>
-            <div class="company-logo-parent">
-                <b class="company-logo">Company Logo</b>
-                <div class="home-parent">
-                    <b class="home">Home</b>
-                    <b class="product" id="productText">Product</b>
-                    <b class="who-we-are" id="whoWeAre">Who we are</b>
-                    <b class="contact-us" id="contactUsText">Contact Us</b>
-                    <b class="signin" id="signInText">SignIn</b>
-                    <div class="shopping-cart-parent">
-                        <div class="shopping-cart">
-                        <i class="fa-solid fa-cart-shopping"></i>
-                        </div>
-                        <b class="cart" id="cartText">Cart</b>
-                    </div>
+            <div className="nav-parent">
+                <img className="company-logo" src={logo} alt="" />
+                <div className="nav-child">
+                    <b className="nav-link"><i className="fa-solid fa-house"></i>Home</b>
+                    <b className="nav-link"><i className="fa-solid fa-circle-info"></i>Who we are</b>
+                    <b className="nav-link"><i className="fa-solid fa-store"></i>Product</b>
+                    <b className="nav-link"><i className="fa-solid fa-users"></i>ContactUs</b>
+                    <b className="nav-link"><i className="fa-solid fa-right-to-bracket"></i>SignIn</b>
+                    <b className="nav-link"><i className="fa-solid fa-cart-shopping"></i>Cart</b>
+                </div>
+                <button className="nav-toggle-button" onClick={toggleDropdown}><i className="fa-solid fa-bars"></i></button>
+                <div className={`nav-dropdown ${dropdownVisible ? 'show' : ''}`}>
+                    <b className="nav-link"><i className="fa-solid fa-house"></i>Home</b>
+                    <b className="nav-link"><i className="fa-solid fa-circle-info"></i>Who we are</b>
+                    <b className="nav-link"><i className="fa-solid fa-store"></i>Product</b>
+                    <b className="nav-link"><i className="fa-solid fa-users"></i>ContactUs</b>
+                    <b className="nav-link"><i className="fa-solid fa-right-to-bracket"></i>SignIn</b>
+                    <b className="nav-link"><i className="fa-solid fa-cart-shopping"></i>Cart</b>
                 </div>
             </div>
         </div>
--- client/src/user/Login.jsx
+++ client/src/user/Login.jsx
@@ -5,26 +5,28 @@ import React from 'react';
 
 function Login() {
   return (
-    <div className="container">
-      <div className="left">
-        <div>Image or color</div>
-      </div>
-      <div className="right">
-        <h2>Login</h2>
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" />
+    <div class="parent-container">
+      <div className="container">
+        <div className="left">
+          <div>Image or color</div>
         </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" name="password" />
+        <div className="right">
+          <h2>Login</h2>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" />
+          </div>
+          <button type="submit">Signin</button>
+          <div className="links">
+            <a href="/Register">New user?</a>
+            <a href="/#">Forget Password?</a>
+          </div>
+          <Link to="/Home" className="login-home-link"><FaArrowLeft />Home</Link>
         </div>
-        <button type="submit">Signin</button>
-        <div className="links">
-          <a href="/Register">New user?</a>
-          <a href="/#">Forget Password?</a>
-        </div>
-        <Link to="/Home" className="login-home-link"><FaArrowLeft />Home</Link>
       </div>
     </div>
   );
