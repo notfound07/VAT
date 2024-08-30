5a1f74e35c23526d41e3693c4f76d2bfd73de516
Aman Bhatt <22680764+notfound07@users.noreply.github.com>
2024-08-30 23:05:35 +0530

    Done

--- client/src/Cart/Cart.jsx
+++ client/src/Cart/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useContext} from 'react';
+import React, { useContext } from 'react';
 import { CartContext } from '../Resources/CartContext';
 import Navbar from '../Nav-Foot/Navbar';
 // import axios from 'axios';
@@ -10,23 +10,6 @@ import { Buffer } from 'buffer';
 const Cart = () => {
   const { cart, removeFromCart } = useContext(CartContext);
   const validCartItems = cart.filter(item => item && item.image && item.title);
-  // const bookings = JSON.parse(localStorage.getItem('cart'));
-
-  // const orderSelected = async (e) => {
-  //   e.preventDefault();
-  //   console.log(bookings);
-  //   try {
-  //     const response = await axios.post('http://localhost:3001/vat/booking',{
-  //       items:bookings}
-  //     );
-  //     console.log(response.data);
-  //     alert('Order placed successfully!');
-  //   } catch (error) {
-  //     console.error('Error placing order:', error);
-  //     alert('Failed to place order');
-  //   }
-  // };
-
   return (
     <div>
       <Navbar />
@@ -35,29 +18,28 @@ const Cart = () => {
         <hr />
       </div>
       <form>
-      <div className="cart-page">
-        {validCartItems.length === 0 ? (
-          <p>Your cart is empty</p>
-        ) : (
-          <ul className="cart-list">
-            {validCartItems.map((item, index) => (
-              <li key={index} className="cart-item">
-                <div className="cart-item-image">
-                  <img src={`data:${item.image.contentType};base64,${Buffer.from(item.image.data.data).toString('base64')}`} 
-                    alt={item.title}  className="cart-img" />
-                </div>
-                <div className="cart-item-details">
-                  <h2 className="cart-item-title">{item.title}</h2>
-                </div>
-                <div className="cart-item-actions">
-                  <button type="button" className="cart-btn" onClick={() => removeFromCart(index)}>Remove</button>
-                </div>
-              </li>
-            ))}
-          </ul>
-        )}
-                {/* <button className="cart-btn" type='submit'>Place Order</button> */}
-      </div>
+        <div className="cart-page">
+          {validCartItems.length === 0 ? (
+            <p>Your cart is empty</p>
+          ) : (
+            <ul className="cart-list">
+              {validCartItems.map((item, index) => (
+                <li key={index} className="cart-item">
+                  <div className="cart-item-image">
+                    <img src={`data:${item.image.contentType};base64,${Buffer.from(item.image.data.data).toString('base64')}`}
+                      alt={item.title} className="cart-img" />
+                  </div>
+                  <div className="cart-item-details">
+                    <h2 className="cart-item-title">{item.title}</h2>
+                  </div>
+                  <div className="cart-item-actions">
+                    <button type="button" className="cart-btn" onClick={() => removeFromCart(index)}>Remove</button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
       </form>
       <Footer />
     </div>
