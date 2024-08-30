047e1aff876ccaadc86d301b3055e62c67f04547
alisbah6 <alisbahhina@gmail.com>
2024-08-30 23:37:29 +0530

    admin_updated

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ function sendEmail({ recipient_email, OTP }) {
 <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
   <div style="margin:50px auto;width:70%;padding:20px 0">
     <div style="border-bottom:1px solid #eee">
-      <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">ReserveIt</a>
+      <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Visionaryart Technology</a>
     </div>
     <p style="font-size:1.1em">Hi,</p>
     <p>Thank you. Use the following OTP to complete your Password Recovery Procedure. OTP is valid for 5 minutes</p>
